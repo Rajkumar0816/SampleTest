e0b14fee6334f1b6dd511a5403e31a4a17ac4c04
rajkumarm <rajkumarm@TAG196>
2020-03-12 15:42:52 +0530

    Test222222

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,8 +9,9 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
-          
+        {        
+
+            Console.WriteLine("Test1");
 
         }
     }
